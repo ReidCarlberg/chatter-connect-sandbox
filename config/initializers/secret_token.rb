fc948d82ba861c9599733e796e1178052d576b4d
ReidCarlberg <reid.carlberg@gmail.com>
2011-09-13 01:02:43 +0100

    initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChatterConnectSandbox::Application.config.secret_token = '4e2a7fd5df26628142407804b895e344009edd7bbe8b493be848a97d0f39764da902c749094910ed44d8be585b0a160c3662b232bfc95603dc9cf40320f60de0'
