f713d146ed1352e2ec17a4e7e16fbd687942cab6
ReidCarlberg <reid.carlberg@gmail.com>
2011-12-12 20:06:18 -0600

    more html

--- config/routes.rb
+++ config/routes.rb
@@ -85,7 +85,7 @@ ChatterConnectSandbox::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "home#index"
+  root :to => "home#index3"
 
   # See how all your routes lay out with "rake routes"
 
